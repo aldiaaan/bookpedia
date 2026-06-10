3461d8b59aeb38c9e7f9bd905a87c80b0460fecb
aldiaaan <aldian.asmr@gmail.com>
2026-06-11 04:12:40 +0700

    feat: add neondb support

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,24 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import pg from "pg"
+import { drizzle as drizzleNodePostgres } from "drizzle-orm/node-postgres"
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless"
+import { Pool as NodePostgresPool } from "pg"
+import { Pool as NeonPool } from "@neondatabase/serverless"
 
+import { singleton } from "@/lib/utils"
 import * as schema from "./schema"
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-})
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is missing!")
+}
 
-export const db = drizzle(pool, { schema })
+const databaseUrl = process.env.DATABASE_URL
+const isNeon = databaseUrl.includes("neon.tech")
+
+export const db = singleton("db", () => {
+  if (isNeon) {
+    const pool = new NeonPool({ connectionString: databaseUrl })
+    return drizzleNeon(pool, { schema })
+  }
+
+  const pool = new NodePostgresPool({ connectionString: databaseUrl })
+  return drizzleNodePostgres(pool, { schema })
+})
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function singleton<T>(name: string, valueFactory: () => T): T {
+  const g = globalThis as unknown as Record<string, T>
+
+  if (!(name in g)) {
+    g[name] = valueFactory()
+  }
+
+  return g[name]
+}
